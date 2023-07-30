ca3564523aa7e9cbe26f4039a82bbb30d706febd
Rune <christian.rune1998@gmail.com>
2023-07-30 21:17:51 +0200

    Update Update-Upgrade-Linux.sh
    Better timer, navigation and text layout.

--- BASH-Script/Quick-Deploy-Script/Update-Upgrade-Linux.sh
+++ BASH-Script/Quick-Deploy-Script/Update-Upgrade-Linux.sh
@@ -18,14 +18,19 @@ sleep 2
 hostnamectl set-hostname $Hostname
 echo "The Hostname Has Been Change To $Hostname"
 sleep 2
+clear
+sleep 10
 
 # Change IP to static via variables
 echo "Change IP To Static"
 read -p 'New IP Address: ' IP
 echo "The IP Has Been Change To $IP"
 sleep 2
+clear
+sleep 10
 
 # Overwrite the /etc/network/interfaces file with new settings
+echo "Overwrite the /etc/network/interfaces file with new settings"
 echo "# This file describes the network interfaces available on your system" > /etc/network/interfaces
 echo "# and how to activate them. For more information, see interfaces(5)." > /etc/network/interfaces
 echo " " > /etc/network/interfaces
@@ -43,6 +48,8 @@ echo " netmask 255.255.255.0" > /etc/network/interfaces
 echo " gateway 192.168.1.1" > /etc/network/interfaces
 echo " dns-nameservers 192.168.1.1 1.1.1.1" > /etc/network/interfaces
 sleep 2
+clear
+sleep 10
 
 # Install Docker Engine, containerd, and Docker Compose.
 
@@ -50,12 +57,16 @@ echo "Update the apt package index and install packages to allow apt to use a re
 apt-get update -y
 apt-get install ca-certificates curl gnupg -y
 sleep 2
+clear
+sleep 10
 
 echo "Add Docker’s official GPG key:"
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 sleep 2
+clear
+sleep 10
 
 echo "Setting up the repository"
 echo \
@@ -63,6 +74,8 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 sleep 2
+clear
+sleep 10
 
 echo "Update the apt package index and Install Docker Engine, containerd, and Docker Compose."
 apt-get update -y
