04a5f8343ec7c74d267fbf82824c30a521b37e43
Rune <christian.rune1998@gmail.com>
2023-07-30 21:23:27 +0200

    Update Update-Upgrade-Linux.sh
    Minor Patch to clear and sleep timings

--- BASH-Script/Quick-Deploy-Script/Update-Upgrade-Linux.sh
+++ BASH-Script/Quick-Deploy-Script/Update-Upgrade-Linux.sh
@@ -13,24 +13,29 @@ clear
 
 # Change hostname via variables
 echo "Change Hostname Via Hostnamectl"
+echo " "
 read -p 'Hostname: ' Hostname
 sleep 2
 hostnamectl set-hostname $Hostname
+echo " "
 echo "The Hostname Has Been Change To $Hostname"
 sleep 2
 clear
-sleep 10
+
 
 # Change IP to static via variables
 echo "Change IP To Static"
+echo " "
 read -p 'New IP Address: ' IP
+echo " "
 echo "The IP Has Been Change To $IP"
 sleep 2
 clear
-sleep 10
 
 # Overwrite the /etc/network/interfaces file with new settings
 echo "Overwrite the /etc/network/interfaces file with new settings"
+echo " "
+sleep 2
 echo "# This file describes the network interfaces available on your system" > /etc/network/interfaces
 echo "# and how to activate them. For more information, see interfaces(5)." > /etc/network/interfaces
 echo " " > /etc/network/interfaces
@@ -54,30 +59,34 @@ sleep 10
 # Install Docker Engine, containerd, and Docker Compose.
 
 echo "Update the apt package index and install packages to allow apt to use a repository over HTTPS"
+echo " "
+sleep 2
 apt-get update -y
 apt-get install ca-certificates curl gnupg -y
 sleep 2
 clear
-sleep 10
 
 echo "Add Docker’s official GPG key:"
+echo " "
+sleep 2
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 chmod a+r /etc/apt/keyrings/docker.gpg
 sleep 2
 clear
-sleep 10
 
 echo "Setting up the repository"
+echo " "
+sleep 2
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 sleep 2
 clear
-sleep 10
 
 echo "Update the apt package index and Install Docker Engine, containerd, and Docker Compose."
+sleep 2
 apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sleep 2
