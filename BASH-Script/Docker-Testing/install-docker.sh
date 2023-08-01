7ec7959e468233a663c60154fdf88622cf72f2fe
Rune004 <christian.rune1998@gmail.com>
2023-08-01 02:47:53 +0200

    Minor Updates

---
+++ BASH-Script/Docker-Testing/install-docker.sh
@@ -0,0 +1,96 @@
+## |--------------------------------|
+## |       Made By Rune004          |
+## |    Need Support or Help?       |
+## |    It can be found below.      |
+## |                                |
+## |            Discord:            |
+## | https://discord.gg/UHd4tJg9Vm  |
+## |                                |
+## |            Github:             |
+## |  https://github.com/rune004    |
+## |--------------------------------|
+#!/usr/bin/env bash
+clear 
+
+# Bash Text Variables
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+
+# Script Auto Self Update
+echo -e "${BLUE} Script Self Updating ${ENDCOLOR}"
+VERSION="0.0.1"
+SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Quick-Deploy-Script/Quick-Deployer.sh'
+SCRIPT_DESCRIPTION=""
+SCRIPT_LOCATION="${BASH_SOURCE[@]}"
+
+rm -f updater.sh
+
+function update()
+{
+    TMP_FILE=$(mktemp -p "" "XXXXX.sh")
+    curl -s -O "SCRIPT_URL" > "TMP_FILE"
+    NEW_VER=$(grep "^VERSION" "TMP_FILE" | awk -F'[="]' '{print }')
+    ABS_SCRIPT_PATH=$(readlink -f "SCRIPT_LOCATION")
+    if [ "VERSION" \< "NEW_VER" ]
+    then
+        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "VERSION" "NEW_VER"
+
+        echo "cp \"TMP_FILE\" \"ABS_SCRIPT_PATH\"" > updater.sh
+        echo "rm -f \"TMP_FILE\"" >> updater.sh
+        echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> updater.sh
+        echo "exec \"ABS_SCRIPT_PATH\" \"$@\"" >> updater.sh
+
+        chmod +x updater.sh
+        chmod +x "TMP_FILE"
+        exec updater.sh
+    else
+        rm -f "TMP_FILE"
+    fi
+}
+
+update "$@"
+
+echo "$@"
+
+echo -e "${GREEN} Script Self Updating Complete ${ENDCOLOR}"
+echo -e "${YELLOW} Current Script Version VERSION ${ENDCOLOR}"
+
+# Install Docker Engine, containerd, and Docker Compose.
+
+echo "${YELLOW}Update the apt package index and install packages to allow apt to use a repository over HTTPS${ENDCOLOR}"
+echo " "
+sleep 2
+apt-get update -y
+apt-get install ca-certificates curl gnupg -y
+sleep 2
+clear
+
+echo "${YELLOW}Add Docker’s official GPG key:${ENDCOLOR}"
+echo " "
+sleep 2
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+sleep 2
+clear
+
+echo "${YELLOW}Setting up the repository${ENDCOLOR}"
+echo " "
+sleep 2
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+sleep 2
+clear
+
+echo "${YELLOW}Update the apt package index and Install Docker Engine, containerd, and Docker Compose.${ENDCOLOR}"
+echo " "
+sleep 2
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sleep 2
