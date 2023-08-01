3754d5257f23184b3202b09ca64b6eea647a30ee
Rune004 <christian.rune1998@gmail.com>
2023-08-01 06:11:42 +0200

    Added Automated Updates

--- webscrapers/proshop-pull.sh
+++ webscrapers/proshop-pull.sh
@@ -1,4 +1,62 @@
-#!/bin/bash  
+## |--------------------------------|
+## |       Made By Rune004          |
+## |    Need Support or Help?       |
+## |    It can be found below.      |
+## |                                |
+## |            Discord:            |
+## | https://discord.gg/UHd4tJg9Vm  |
+## |                                |
+## |            Github:             |
+## |  https://github.com/rune004    |
+## |--------------------------------|
+#!/usr/bin/env bash
+clear 
+
+# Bash Text Variables
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+
+# Script Auto Self Update
+echo -e "${BLUE}Script Self Updating${ENDCOLOR}"
+VERSION="0.0.1"
+SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/'
+SCRIPT_DESCRIPTION=""
+SCRIPT_LOCATION="${BASH_SOURCE[@]}"
+
+rm -f updater.sh
+
+function update()
+{
+    TMP_FILE=$(mktemp -p "" "XXXXX.sh")
+    curl -s -O "$SCRIPT_URL" > "$TMP_FILE"
+    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+    ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
+    if [ "$VERSION" \< "$NEW_VER" ]
+    then
+        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
+
+        echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > updater.sh
+        echo "rm -f \"$TMP_FILE\"" >> updater.sh
+        echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> updater.sh
+        echo "exec \"$ABS_SCRIPT_PATH\" \"$@\"" >> updater.sh
+
+        chmod +x updater.sh
+        chmod +x "$TMP_FILE"
+        exec updater.sh
+    else
+        rm -f "$TMP_FILE"
+    fi
+}
+
+update "$@"
+
+echo "$@"
+
+echo -e "${GREEN}Script Self Updating Complete${ENDCOLOR}"
+echo -e "${YELLOW}Current Script Version $VERSION${ENDCOLOR}"
    
 echo "Kopier dit proshop link her"  
 # Take input without defining variable    
