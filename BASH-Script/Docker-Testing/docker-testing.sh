ca1638e2ffbdf47d3ba2fe41846cf8ea00dcf0cc
Rune004 <christian.rune1998@gmail.com>
2023-08-01 03:43:16 +0200

    Update

--- BASH-Script/Docker-Testing/docker-testing.sh
+++ BASH-Script/Docker-Testing/docker-testing.sh
@@ -21,7 +21,7 @@ ENDCOLOR="\e[0m"
 
 # Script Auto Self Update
 echo -e "${BLUE}Script Self Updating${ENDCOLOR}"
-VERSION="0.0.2"
+VERSION="0.0.3"
 SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/docker-testing.sh'
 SCRIPT_DESCRIPTION=""
 SCRIPT_LOCATION="${BASH_SOURCE[@]}"
@@ -59,13 +59,13 @@ echo -e "${GREEN}Script Self Updating Complete${ENDCOLOR}"
 echo -e "${YELLOW}Current Script Version $VERSION${ENDCOLOR}"
 
 echo -e "${YELLOW}Make sure you are in the same folder as the script${ENDCOLOR}"
-PS3='Please pick what store you want to add a link to: '
-options=("Add Proshop Link" "Option 2" "Option 3" "Quit")
+PS3='Please pick what docker you want for testing: '
+options=("Debian 10" "Debian 11" "Ubuntu 22.04" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Add Proshop Link")
-            ssh -t root@192.168.1.191 bash -l
+        "Debian 10")
+            ssh -t root@192.168.1.191 ksh bash -l
             sleep 2
             docker run -td --name="Debian-10" debian:10
             sleep 2
@@ -75,10 +75,10 @@ do
             curl -s -O https://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@$url
             break
             ;;
-        "Option 2")
+        "Debian 11")
             echo "$user:$pass"
             ;;
-        "Option 3")
+        "Ubuntu 22.04")
             echo "you chose choice $REPLY which is $opt"
             ;;
         "Quit")
