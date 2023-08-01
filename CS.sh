d26da398b0b99fe2e9c4e402bb3c1b8400a311fe
Rune004 <christian.rune1998@gmail.com>
2023-08-01 09:13:45 +0200

    Removed -s args

--- CS.sh
+++ CS.sh
@@ -70,17 +70,17 @@ DEB_PACKAGE_NAME="python2.7 python-pip libssl-dev"
 
  if cat /etc/*release | grep ^NAME | grep Ubuntu; then
     sudo apt-get update 
-    sudo apt-get install -s -y $DEB_PACKAGE_NAME
+    sudo apt-get install -y $DEB_PACKAGE_NAME
     sudo apt-get install curl -s -y
-    sudo apt-get install -s nano -y
-    sudo apt-get install -s wget -y  
+    sudo apt-get install nano -y
+    sudo apt-get install wget -y  
     sudo apt-get install cron -y 
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
     apt-get update 
-    apt-get install -s -y $DEB_PACKAGE_NAME 
-    apt-get install -s curl -y
-    apt-get install -s nano -y
-    apt-get install -s wget -y 
+    apt-get install -y $DEB_PACKAGE_NAME 
+    apt-get install curl -y
+    apt-get install nano -y
+    apt-get install wget -y 
     apt-get install cron -y
  else
     echo "OS NOT DETECTED, couldn't install package $PACKAGE"
