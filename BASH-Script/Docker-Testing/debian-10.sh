7e794c0e01a9c0213c84736226e6311930ec85c4
Rune004 <christian.rune1998@gmail.com>
2023-08-01 04:39:01 +0200

    Update version

--- BASH-Script/Docker-Testing/debian-10.sh
+++ BASH-Script/Docker-Testing/debian-10.sh
@@ -21,7 +21,7 @@ ENDCOLOR="\e[0m"
 
 # Script Auto Self Update
 echo -e "${BLUE}Script Self Updating${ENDCOLOR}"
-VERSION="0.0.4"
+VERSION="0.0.5"
 SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/debian-10.sh'
 SCRIPT_DESCRIPTION=""
 SCRIPT_LOCATION="${BASH_SOURCE[@]}"
