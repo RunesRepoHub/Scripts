40f31916f3152713bc84d9c55cf3713a425871c1
Rune004 <christian.rune1998@gmail.com>
2023-08-01 04:10:43 +0200

    Minor Patch to error in IP

--- BASH-Script/Docker-Testing/docker-testing.sh
+++ BASH-Script/Docker-Testing/docker-testing.sh
@@ -21,7 +21,7 @@ ENDCOLOR="\e[0m"
 
 # Script Auto Self Update
 echo -e "${BLUE}Script Self Updating${ENDCOLOR}"
-VERSION="0.0.5"
+VERSION="0.0.6"
 SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/docker-testing.sh'
 SCRIPT_DESCRIPTION=""
 SCRIPT_LOCATION="${BASH_SOURCE[@]}"
@@ -66,7 +66,8 @@ do
     case $opt in
         "Debian 10")
             curl -s -O https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/debian-10.sh
-            ssh tools@192.168.56.103 'bash -s' < debian-10.sh
+            sleep 2
+            ssh root@192.168.1.191 'bash -s' < debian-10.sh
             break
             ;;
         "Debian 11")
