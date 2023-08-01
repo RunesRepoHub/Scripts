60c0807bf4b68adfc921a62ee91605c776ab2deb
Rune004 <christian.rune1998@gmail.com>
2023-08-01 02:59:51 +0200

    Minor Changes

--- BASH-Script/Docker-Testing/docker-testing.sh
+++ BASH-Script/Docker-Testing/docker-testing.sh
@@ -21,7 +21,7 @@ ENDCOLOR="\e[0m"
 
 # Script Auto Self Update
 echo -e "${BLUE}Script Self Updating${ENDCOLOR}"
-VERSION="0.1.6"
+VERSION="0.0.1"
 SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/docker-testing.sh'
 SCRIPT_DESCRIPTION=""
 SCRIPT_LOCATION="${BASH_SOURCE[@]}"
@@ -31,23 +31,23 @@ rm -f updater.sh
 function update()
 {
     TMP_FILE=$(mktemp -p "" "XXXXX.sh")
-    curl -s -O "SCRIPT_URL" > "TMP_FILE"
-    NEW_VER=$(grep "^VERSION" "TMP_FILE" | awk -F'[="]' '{print }')
-    ABS_SCRIPT_PATH=$(readlink -f "SCRIPT_LOCATION")
-    if [ "VERSION" \< "NEW_VER" ]
+    curl -s -O "$SCRIPT_URL" > "$TMP_FILE"
+    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+    ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
+    if [ "$VERSION" \< "$NEW_VER" ]
     then
-        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "VERSION" "NEW_VER"
+        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
 
-        echo "cp \"TMP_FILE\" \"ABS_SCRIPT_PATH\"" > updater.sh
-        echo "rm -f \"TMP_FILE\"" >> updater.sh
+        echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > updater.sh
+        echo "rm -f \"$TMP_FILE\"" >> updater.sh
         echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> updater.sh
-        echo "exec \"ABS_SCRIPT_PATH\" \"$@\"" >> updater.sh
+        echo "exec \"$ABS_SCRIPT_PATH\" \"$@\"" >> updater.sh
 
         chmod +x updater.sh
-        chmod +x "TMP_FILE"
+        chmod +x "$TMP_FILE"
         exec updater.sh
     else
-        rm -f "TMP_FILE"
+        rm -f "$TMP_FILE"
     fi
 }
 
@@ -56,7 +56,7 @@ update "$@"
 echo "$@"
 
 echo -e "${GREEN}Script Self Updating Complete${ENDCOLOR}"
-echo -e "${YELLOW}Current Script Version VERSION${ENDCOLOR}"
+echo -e "${YELLOW}Current Script Version $VERSION${ENDCOLOR}"
 
     PS3='Please enter your choice: '
     options=("Debian 10" "Debian 11" "Ubuntu 22.04" "Quit")
--- BASH-Script/Docker-Testing/install-docker.sh
+++ BASH-Script/Docker-Testing/install-docker.sh
@@ -22,7 +22,7 @@ ENDCOLOR="\e[0m"
 # Script Auto Self Update
 echo -e "${BLUE} Script Self Updating ${ENDCOLOR}"
 VERSION="0.0.1"
-SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Quick-Deploy-Script/Quick-Deployer.sh'
+SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/install-docker.sh'
 SCRIPT_DESCRIPTION=""
 SCRIPT_LOCATION="${BASH_SOURCE[@]}"
 
@@ -31,23 +31,23 @@ rm -f updater.sh
 function update()
 {
     TMP_FILE=$(mktemp -p "" "XXXXX.sh")
-    curl -s -O "SCRIPT_URL" > "TMP_FILE"
-    NEW_VER=$(grep "^VERSION" "TMP_FILE" | awk -F'[="]' '{print }')
-    ABS_SCRIPT_PATH=$(readlink -f "SCRIPT_LOCATION")
-    if [ "VERSION" \< "NEW_VER" ]
+    curl -s -O "$SCRIPT_URL" > "$TMP_FILE"
+    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+    ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
+    if [ "$VERSION" \< "$NEW_VER" ]
     then
-        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "VERSION" "NEW_VER"
+        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
 
-        echo "cp \"TMP_FILE\" \"ABS_SCRIPT_PATH\"" > updater.sh
-        echo "rm -f \"TMP_FILE\"" >> updater.sh
+        echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > updater.sh
+        echo "rm -f \"$TMP_FILE\"" >> updater.sh
         echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> updater.sh
-        echo "exec \"ABS_SCRIPT_PATH\" \"$@\"" >> updater.sh
+        echo "exec \"$ABS_SCRIPT_PATH\" \"$@\"" >> updater.sh
 
         chmod +x updater.sh
-        chmod +x "TMP_FILE"
+        chmod +x "$TMP_FILE"
         exec updater.sh
     else
-        rm -f "TMP_FILE"
+        rm -f "$TMP_FILE"
     fi
 }
 
@@ -55,8 +55,8 @@ update "$@"
 
 echo "$@"
 
-echo -e "${GREEN} Script Self Updating Complete ${ENDCOLOR}"
-echo -e "${YELLOW} Current Script Version VERSION ${ENDCOLOR}"
+echo -e "${GREEN}Script Self Updating Complete${ENDCOLOR}"
+echo -e "${YELLOW}Current Script Version $VERSION${ENDCOLOR}"
 
 # Install Docker Engine, containerd, and Docker Compose.
 
