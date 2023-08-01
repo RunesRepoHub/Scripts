8533b7557c6f491308e79687437f5980f80512d7
Rune004 <christian.rune1998@gmail.com>
2023-08-01 22:45:46 +0200

    Hotfix

--- Run-CnC.sh
+++ Run-CnC.sh
@@ -104,30 +104,30 @@ then
     echo "Choose an option:"
     select opt in "${options[@]}"; do
         case $REPLY in
-            "Webscrapers")
+            "Webscrapers" 1)
                 # Export User/Pass for auth header in webscrapers
                 export user="$userdb"
                 export pass="$passdb"
                 bash ./Scripts/Sub-menu/Webscrapers/webscrapers.sh
                 break
                 ;;
-            "Make a Virtual Machine")
+            "Make a Virtual Machine" 2)
                 bash ./Scripts/Sub-menu/Make-VM/makevm.sh
                 break
                 ;;
-            "Reboot Restart N8N")
+            "Reboot Restart N8N" 3)
                 bash ./Scripts/Sub-menu/N8N/n8n-CnC.sh
                 break
                 ;;
-            "Docker Testing")
+            "Docker Testing" 4)
                 bash ./Scripts/Sub-menu/Docker-Testing/docker-testing.sh
                 break
                 ;;
-            "Add Midnight Cron")
+            "Add Midnight Cron" 5)
                 bash ./Scripts/CnC/add-midnight-cron.sh
                 break
                 ;;
-            "Quit")
+            "Quit" 6)
                 break
                 ;;
             *) echo "invalid option $REPLY";;
