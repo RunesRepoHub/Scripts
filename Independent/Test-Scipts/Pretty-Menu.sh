d4e0241ffbe14213d6f0a809ba49e9a2a441a045
Rune004 <christian.rune1998@gmail.com>
2023-08-02 20:55:32 +0200

    New test for pretty menu

--- Independent/Test-Scipts/Pretty-Menu.sh
+++ Independent/Test-Scipts/Pretty-Menu.sh
@@ -1,18 +1,34 @@
-#!/bin/bash
-        
-options=("Install Docker & Docker-Compose" "Cloudflare" "Restart N8N MySQL" "Quit")
-    dialog --menu "Menu" 10 30 3 1 Install Docker + Docker-Compose 2 Cloudflare 3 Quit
-        case $? in
-        1)
-            bash ./Scripts/Installers/install-docker.sh
-            break
-            ;;
-        2)
-            bash ./Scripts/Installers/install-cloudflare-tunnel.sh
-            break
-            ;;
-        3)
-            break 2
-            ;;
-        *) echo -e "${RED}invalid option $REPLY${ENDCOLOR}";;
-        esac
+#!/bin/sh
+dialog --menu "List file of a directory" 10 30 3 1 /home 2 /root 3 /tmp 2>temp
+ 
+ 
+# OK is pressed
+if [ "$?" = "0" ]
+then
+        _return=$(cat temp)
+ 
+        # /home is selected
+        if [ "$_return" = "1" ]
+        then
+                dialog --title "List file of directory /home" --msgbox "$(ls /home -l)" 100 100
+        fi
+ 
+         # /root is selected
+        if [ "$_return" = "2" ]
+        then
+                dialog --title "List file of directory /root" --msgbox "$(ls /root -l)" 100 100
+        fi
+ 
+         # /tmp is selected
+        if [ "$_return" = "3" ]
+        then
+                dialog --title "List file of directory /tmp" --msgbox "$(ls /tmp -l)" 100 100
+        fi
+ 
+# Cancel is pressed
+else
+        echo "Cancel is pressed"
+fi
+ 
+# remove the temp file
+rm -f temp
