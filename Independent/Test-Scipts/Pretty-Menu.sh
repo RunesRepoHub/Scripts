6260c4be328f908fc4d43561fe068fda9055d223
Rune004 <christian.rune1998@gmail.com>
2023-08-02 20:26:25 +0200

    New test for Pretty-menu

--- Independent/Test-Scipts/Pretty-Menu.sh
+++ Independent/Test-Scipts/Pretty-Menu.sh
@@ -2,13 +2,25 @@
 DIALOG=${DIALOG=dialog}
 
 $DIALOG --title " My first dialog" --clear \
-        --yesno "Hello , this is my first dialog program" 10 30
+        --input "Hello , this is my first dialog program" 10 30
+
 
 case $? in
-  0)
-    echo "Yes chosen.";;
-  1)
-    echo "No chosen.";;
-  255)
-    echo "ESC pressed.";;
+        1)
+            bash ./Scripts/Installers/install-docker.sh
+            break
+            ;;
+        2)
+            bash ./Scripts/Installers/install-cloudflare-tunnel.sh
+            break
+            ;;
+        3)
+            curl -s GET "https://n8n-b.rp-helpdesk.com/webhook/reboot-N8N-mysql"
+            echo " "
+            break
+            ;;
+        4)
+            break 2
+            ;;
+        *) echo -e "${RED}invalid option $REPLY${ENDCOLOR}";;
 esac
