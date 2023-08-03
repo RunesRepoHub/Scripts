579c63981fbba54a5ade8e231a8c87b11778a979
Rune004 <christian.rune1998@gmail.com>
2023-08-03 22:31:21 +0200

    Minor test to login

--- Security/Login-Script.sh
+++ Security/Login-Script.sh
@@ -13,10 +13,18 @@ pass=$(\
 # Check Users Login With Database
 userdb=$(curl -s --user $user:$pass https://n8n-b.rp-helpdesk.com/webhook/login-user)
 passdb=$(curl -s --user $user:$pass https://n8n-b.rp-helpdesk.com/webhook/login-pass)
+token=$(curl -s --user $user:$pass https://n8n-b.rp-helpdesk.com/webhook/token)
+sessionid=$(curl -s --user $user:$pass https://n8n-b.rp-helpdesk.com/webhook/sessionid)
 
+export user="$user"
+export pass="$pass"
+export userdb="$userdb"
+export passdb="$passdb"
+export token="$token"
+export sessionid="$sessionid"
 
 # Check the username and password are valid or not
-if (( $user == "$userdb" && $pass == "$passdb" ))
+if (( $user == "$userdb" && $pass == "$passdb" && $sessionid == "$sessionid" ))
 then
     bash ./Scripts/Menu/Main-Menu.sh
 else 
