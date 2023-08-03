ea2a196224496c3678ad2953e03aba374fb1aacd
Rune004 <christian.rune1998@gmail.com>
2023-08-03 04:53:39 +0200

    Testing

--- Sub-menu/Docker-Testing/Ubuntu-22-04.sh
+++ Sub-menu/Docker-Testing/Ubuntu-22-04.sh
@@ -9,65 +9,14 @@
 ## |            Github:             |
 ## |  https://github.com/rune004    |
 ## |--------------------------------|
-#!/usr/bin/env bash
-clear 
+SCRIPT="Ubuntu-22-04.sh"
 
-# Bash Text Variables
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-ENDCOLOR="\e[0m"
-SCRIPTNAME="Ubuntu-22.04"
-
-# Script Auto Self Update
-echo -e "${BLUE}Script $SCRIPTNAME Updating${ENDCOLOR}"
-VERSION="0.0.1"
-SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Docker-Testing/Ubuntu-22-04.sh'
-SCRIPT_DESCRIPTION=""
-SCRIPT_LOCATION="${BASH_SOURCE[@]}"
-FILE_UPDATER=updater.sh
-
-rm -f "$FILE_UPDATER"
-
-function update()
-{
-    TMP_FILE=$(mktemp -p "" "XXXXX.sh")
-    curl -s -O "$SCRIPT_URL" > "$TMP_FILE"
-    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
-    ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
-    if [ "$VERSION" \< "$NEW_VER" ]
-    then
-        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
-
-        echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > "$FILE_UPDATER"
-        echo "rm -f \"$TMP_FILE\"" >> "$FILE_UPDATER"
-        echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> "$FILE_UPDATER"
-        echo "exec \"$ABS_SCRIPT_PATH\" \"$@\"" >> "$FILE_UPDATER"
-
-        chmod +x "$FILE_UPDATER"
-        chmod +x "$TMP_FILE"
-        exec "$FILE_UPDATER"
-    else
-        rm -f "$TMP_FILE"
-    fi
-}
-
-update "$@"
-
-echo "$@"
-
-echo -e "${GREEN}Script $SCRIPTNAME Updating Complete${ENDCOLOR}"
-echo -e "${YELLOW}Current Script Version $VERSION${ENDCOLOR}"
-
-echo -e "${GREEN}${ENDCOLOR}"
-
-docker stop $SCRIPTNAME
-docker rm $SCRIPTNAME
+docker stop $SCRIPT
+docker rm $SCRIPT
 docker volume prune -y
 
-docker run -i -d --name="$SCRIPTNAME" ubuntu:22.04
-docker exec -i $SCRIPTNAME /bin/bash
+docker run -i -d --name="$SCRIPT" ubuntu:22.04
+docker exec -i $SCRIPT /bin/bash
 apt-get update
 apt-get upgrade -y
 apt-get install curl -y
