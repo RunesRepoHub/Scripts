fe8a344ef53c5d47ad272715ac66d10fe925ff8b
Rune004 <christian.rune1998@gmail.com>
2023-08-03 03:43:49 +0200

    Testing

--- Sub-menu/N8N/n8n-CnC.sh
+++ Sub-menu/N8N/n8n-CnC.sh
@@ -9,51 +9,7 @@
 ## |            Github:             |
 ## |  https://github.com/rune004    |
 ## |--------------------------------|
-#!/usr/bin/env bash
-clear 
 
-# Bash Text Variables
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-ENDCOLOR="\e[0m"
-SCRIPTNAME="n8n-CnC.sh"
-
-# Script Auto Self Update
-VERSION="0.0.2"
-SCRIPT_URL='https://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@raw.githubusercontent.com/rune004/Scripts/main/Sub-menu/N8N/n8n-CnC.sh'
-SCRIPT_DESCRIPTION=""
-SCRIPT_LOCATION="${BASH_SOURCE[@]}"
-FILE_UPDATER=updater.sh
-
-rm -f "$FILE_UPDATER"
-
-function update()
-{
-    TMP_FILE=$(mktemp -p "" "XXXXX.sh")
-    curl -s -O "$SCRIPT_URL" > "$TMP_FILE"
-    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
-    ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
-    if [ "$VERSION" \< "$NEW_VER" ]
-    then
-        printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
-
-        echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > "$FILE_UPDATER"
-        echo "rm -f \"$TMP_FILE\"" >> "$FILE_UPDATER"
-        echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> "$FILE_UPDATER"
-        echo "exec \"$ABS_SCRIPT_PATH\" \"$@\"" >> "$FILE_UPDATER"
-
-        chmod +x "$FILE_UPDATER"
-        chmod +x "$TMP_FILE"
-        exec "$FILE_UPDATER"
-    else
-        rm -f "$TMP_FILE"
-    fi
-}
-
-update "$@"
-echo "$@"
 
 
 # N8N CnC Menu
