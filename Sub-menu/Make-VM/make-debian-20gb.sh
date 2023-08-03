7534688bd9775f9eefe8d9b29da40b184e1a56b4
Rune004 <christian.rune1998@gmail.com>
2023-08-03 22:51:08 +0200

    Hotfix

--- Sub-menu/Make-VM/make-debian-20gb.sh
+++ Sub-menu/Make-VM/make-debian-20gb.sh
@@ -20,10 +20,18 @@ hostname=$(\
   3>&1 1>&2 2>&3 3>&- \
 )
 
-dialog --infobox "Your Virtual Machine Is Now Being Made" 10 30 ; sleep 5
+dialog --infobox "Your Virtual Machine Is Now Being Made" 10 30 ; sleep 3
 
 curl -X POST "https://n8n-prod.rp-helpdesk.com/webhook/K3s-boot-linux?hostname=$hostname"
-sleep 180
+dialog --infobox "Your Virtual Machine Is Now Being Made - 5 mins left" 10 30 ; sleep 60
+dialog --infobox "Your Virtual Machine Is Now Being Made - 4 mins left" 10 30 ; sleep 60
+dialog --infobox "Your Virtual Machine Is Now Being Made - 3 mins left" 10 30 ; sleep 60
+dialog --infobox "Your Virtual Machine Is Now Being Made - 2 mins left" 10 30 ; sleep 60
+dialog --infobox "Your Virtual Machine Is Now Being Made - 1 mins left" 10 30 ; sleep 30
+dialog --infobox "Your Virtual Machine Is Now Being Made - 30 sekundes left" 10 30 ; sleep 10
+dialog --infobox "Your Virtual Machine Is Now Being Made - 20 sekundes left" 10 30 ; sleep 10
+dialog --infobox "Your Virtual Machine Is Now Being Made - 10 sekundes left" 10 30 ; sleep 5
+dialog --infobox "Your Virtual Machine Is Now Being Made - 5 sekundes left" 10 30 ; sleep 5
 
 IP="$(curl -s GET https://n8n-prod.rp-helpdesk.com/webhook/ip)"
 
