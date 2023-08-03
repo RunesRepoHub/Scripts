868804cd25fc8c266266784edfee2a43120b35f1
Rune004 <christian.rune1998@gmail.com>
2023-08-03 23:23:44 +0200

    Testing

--- Installers/install-cloudflare-tunnel.sh
+++ Installers/install-cloudflare-tunnel.sh
@@ -21,7 +21,7 @@ TUNNELNAME=$(\
   3>&1 1>&2 2>&3 3>&- \
 )
 
-dialog --programbox "docker run -it --rm -v /mnt/user/appdata/cloudflared:/home/nonroot/.cloudflared/ cloudflare/cloudflared:latest tunnel create "$TUNNELNAME""
+dialog --programbox "(docker run -it --rm -v /mnt/user/appdata/cloudflared:/home/nonroot/.cloudflared/ cloudflare/cloudflared:latest tunnel create "$TUNNELNAME")" 20 40 \
 
 touch /mnt/user/appdata/cloudflared/config.yml
 
