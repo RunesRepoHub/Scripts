8a89492542ff6094c6ddb4ec3d3289e84f9efd07
Rune004 <christian.rune1998@gmail.com>
2023-08-04 02:22:04 +0200

    testing

--- Sub-menu/Make-VM/make-debian-20gb.sh
+++ Sub-menu/Make-VM/make-debian-20gb.sh
@@ -24,50 +24,15 @@ dialog --infobox "Your Virtual Machine Is Now Being Made" 10 30 ; sleep 3
 
 curl -X POST "https://n8n-prod.rp-helpdesk.com/webhook/K3s-boot-linux?hostname=$hostname"
 
-dialog --title "$me" \
-       --gauge "5 mins left" 10 30 5 ; 
-sleep 60
-clear
-
-dialog --title "$me" \
-       --gauge "4 mins left" 10 30 15 ; 
-sleep 60
-clear
-
-dialog --title "$me" \
-       --gauge "3 mins left" 10 30 35 ; 
-sleep 60
-clear
-
-dialog --title "$me" \
-       --gauge "2 mins left" 10 30 55 ; 
-sleep 60
-clear
-
-dialog --title "$me" \
-       --gauge "1 mins left" 10 30 75 ; 
-sleep 30
-clear
-
-dialog --title "$me" \
-       --gauge "30 sekundes left" 10 30 85 ; 
-sleep 10
-clear
-
-dialog --title "$me" \
-       --gauge "20 sekundes left" 10 30 90 ; 
-sleep 10
-clear
-
-dialog --title "$me" \
-       --gauge "10 sekundes left" 10 30 95 ; 
-sleep 5
-clear
-
-dialog --title "$me" \
-       --gauge "5 sekundes left" 10 30 98 ; 
-sleep 5
-clear
+dialog --gauge "5 mins left" 10 30 5 ; sleep 60
+dialog --gauge "4 mins left" 10 30 15 ; sleep 60
+dialog --gauge "3 mins left" 10 30 35 ; sleep 60
+dialog --gauge "2 mins left" 10 30 55 ; sleep 60
+dialog --gauge "1 mins left" 10 30 75 ; sleep 30
+dialog --gauge "30 sekundes left" 10 30 85 ; sleep 10
+dialog --gauge "20 sekundes left" 10 30 90 ; sleep 10
+dialog --gauge "10 sekundes left" 10 30 95 ; sleep 5
+dialog --gauge "5 sekundes left" 10 30 98 ; sleep 5
 
 IP="$(curl -s GET https://n8n-prod.rp-helpdesk.com/webhook/ip)"
 
