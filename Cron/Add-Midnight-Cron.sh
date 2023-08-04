4db209c1bbd293644434903ab780f44db9b35267
Rune004 <christian.rune1998@gmail.com>
2023-08-04 11:03:41 +0200

    Testing

--- Cron/Add-Midnight-Cron.sh
+++ Cron/Add-Midnight-Cron.sh
@@ -28,12 +28,12 @@ DEB_PACKAGE_NAME="python2.7 python-dev libssl-dev"
     dialog --backtitle "$scriptname - Version $version" --title " $me " --prgbox "Sudo mkdir /opt/Scripts" "sudo mkdir /opt/Scripts" 30 60
     # Copy file midnight-midget.sh to a "commen" system location
     dialog --backtitle "$scriptname - Version $version" --title " $me " --prgbox "Curl Midnight-Midget" "sudo curl -s -O https://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@raw.githubusercontent.com/rune004/Scripts/main/BASH-Script/Cron/midnight-midget.sh" 30 60
-    dialog --backtitle "$scriptname - Version $version" --title " $me " --prgbox "Copy Midnight-Midget" "sudo cp midnight-midget.sh /opt/Scripts/midnight-midget.sh" 30 60
-    dialog --backtitle "$scriptname - Version $version" --title " $me " --prgbox "Remove Midnight-Midget Template" "sudo rm midnight-midget.sh" 30 60
+    sudo cp midnight-midget.sh /opt/Scripts/midnight-midget.sh
+    sudo rm midnight-midget.sh
     # Command to add a symbolic link in /usr/bin/
-    dialog --backtitle "$scriptname - Version $version" --title " $me " --prgbox "Sudo ln" "sudo ln -s /opt/Scripts/midnight-midget.sh /usr/bin/" 30 60
+    sudo ln -s /opt/Scripts/midnight-midget.sh /usr/bin/
     # Setup cron settings
-    dialog --backtitle "$scriptname - Version $version" --title " $me " --prgbox "Crontab" "sudo crontab -l > file; echo '00 00 * * * ruby /opt/Scripts/midnight-midget.sh >/dev/null 2>&1' >> file; crontab file"
+    sudo crontab -l > file; echo '00 00 * * * ruby /opt/Scripts/midnight-midget.sh >/dev/null 2>&1' >> file; crontab file
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
     apt-get update
     apt-get install -y $DEB_PACKAGE_NAME
