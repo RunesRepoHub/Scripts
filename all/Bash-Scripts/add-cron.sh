296d58506249e5e36cdecac3cde2e6fb6c6395c2
Rune004 <christian.rune1998@gmail.com>
2023-08-15 09:44:08 +0200

    Hotfix

--- all/Bash-Scripts/add-cron.sh
+++ all/Bash-Scripts/add-cron.sh
@@ -1,7 +1,7 @@
-!/bin/bash
+#!/bin/bash
 crontab -l > crontab_new 
 echo "2 0 * * * ~/Scripts/all/Bash-Scripts/testing.sh" >> crontab_new
 crontab crontab_new
 rm crontab_new
 chmod +x ~/Scripts/all/Bash-Scripts/testing.sh
-./myScript.sh
+./testing.sh
