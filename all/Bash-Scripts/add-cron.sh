d0e7f2b4e681ebc15a19b8fef55573d5184ab84b
Rune004 <christian.rune1998@gmail.com>
2023-08-15 10:34:37 +0200

    Minor Patch

--- all/Bash-Scripts/add-cron.sh
+++ all/Bash-Scripts/add-cron.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-crontab -l > crontab_new 
-echo "2 0 * * * ~/Scripts/all/Bash-Scripts/testing.sh" >> crontab_new
-crontab crontab_new
-rm crontab_new
-chmod +x ~/Scripts/all/Bash-Scripts/testing.sh
-./testing.sh
+sudo /bin/bash -c 'echo "30 4 * * * root ~/Scripts/all/Bash-Scripts/midnight-midget.sh" >> /etc/crontab'
+chmod +x ~/Scripts/all/Bash-Scripts/midnight-midget.sh
