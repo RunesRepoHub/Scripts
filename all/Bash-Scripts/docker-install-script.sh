48f7ea58239d48dca65bfb2ae51cae72b6fe47ea
rune <christian.rune1998@gmail.com>
2023-10-14 13:37:02 +0200

    Minor changes to docker install script

--- all/Bash-Scripts/docker-install-script.sh
+++ all/Bash-Scripts/docker-install-script.sh
@@ -1,59 +1,81 @@
 #!/bin/bash
 
-# Check if the user has root privileges
+# Check if the script is running as root
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root."
-  exit 1
+    echo "Please run this script as root."
+    exit 1
 fi
 
-# Check if the system is Ubuntu or Debian
-if [ -f /etc/lsb-release ]; then
-  # Ubuntu
-  DISTRO=$(lsb_release -cs)
-elif [ -f /etc/debian_version ]; then
-  # Debian
-  DISTRO="debian"
+# Check if lsb_release command is available (Ubuntu/Debian)
+if [ -x "$(command -v lsb_release)" ]; then
+    DISTRO=$(lsb_release -is)
 else
-  echo "Unsupported distribution. This script only supports Ubuntu and Debian."
-  exit 1
+    # If lsb_release is not available, try to identify the distribution based on files
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        DISTRO=$ID
+    else
+        echo "Unsupported distribution. Exiting."
+        exit 1
+    fi
 fi
 
-# Install Docker
-echo "Installing Docker for $DISTRO"
-if [ "$DISTRO" = "debian" ]; then
-  # Debian installation
-  apt-get update
-  apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $DISTRO stable"
-  apt-get update
-  apt-get install -y docker-ce
+# Install Docker and Docker Compose on Ubuntu
+install_docker_ubuntu() {
+    apt-get update
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+    # Add Docker GPG key and repository
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io
+}
+
+# Install Docker and Docker Compose on Debian
+install_docker_debian() {
+    apt-get update
+    apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+    # Add Docker GPG key and repository
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io
+}
+
+# Install Docker Compose
+install_docker_compose() {
+    if [ ! -f /usr/local/bin/docker-compose ]; then
+        curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
+    fi
+}
+
+# Check if Docker is already installed
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is already installed."
+    echo "Updating Docker..."
+    apt-get update
+    apt-get upgrade -y
 else
-  # Ubuntu installation
-  apt-get update
-  apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DISTRO stable"
-  apt-get update
-  apt-get install -y docker-ce
+    echo "Docker is not installed. Installing..."
+    case $DISTRO in
+        "Ubuntu") install_docker_ubuntu ;;
+        "Debian") install_docker_debian ;;
+        *) echo "Unsupported distribution. Exiting."; exit 1 ;;
+    esac
 fi
 
 # Install Docker Compose
-echo "Installing Docker Compose"
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+install_docker_compose
 
-# Install Docker CLI
-echo "Installing Docker CLI"
-apt-get install -y docker-ce-cli
-
-# Start and enable Docker service
-systemctl start docker
-systemctl enable docker
-
-# Print Docker, Docker CLI, and Docker Compose versions
+# Check Docker and Docker Compose versions
 docker --version
 docker-compose --version
 
-echo "Docker, Docker CLI, and Docker Compose have been successfully installed."
+echo "Docker and Docker Compose installation completed."
 
+exit 0
