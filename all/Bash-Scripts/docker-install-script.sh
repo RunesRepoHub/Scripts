0b1e6a8b2ef8c1f9f80930024a170c2dbe21ce47
rune <christian.rune1998@gmail.com>
2023-10-14 13:30:31 +0200

    Modify docker install script to both ubuntu&debian

--- all/Bash-Scripts/docker-install-script.sh
+++ all/Bash-Scripts/docker-install-script.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 
-# Check if the script is run as root or with sudo
+# Check if the user has root privileges
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or using sudo."
+  echo "Please run this script as root."
   exit 1
 fi
 
-# Update the package manager
-apt update
-
-# Install prerequisites
-apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-# Add Docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-# Add Docker repository
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Update the package manager again to refresh the package cache
-apt update
-
-# Install Docker
-apt install -y docker-ce
-
-# Install Docker Compose
-if [ ! -f /usr/local/bin/docker-compose ]; then
-  curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+# Check if the system is Ubuntu or Debian
+if [ -f /etc/lsb-release ]; then
+  # Ubuntu
+  DISTRO=$(lsb_release -cs)
+elif [ -f /etc/debian_version ]; then
+  # Debian
+  DISTRO="debian"
+else
+  echo "Unsupported distribution. This script only supports Ubuntu and Debian."
+  exit 1
 fi
 
-# Add your user to the 'docker' group to run Docker without sudo
-usermod -aG docker $USER
+# Install Docker
+echo "Installing Docker for $DISTRO"
+if [ "$DISTRO" = "debian" ]; then
+  # Debian installation
+  apt-get update
+  apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $DISTRO stable"
+  apt-get update
+  apt-get install -y docker-ce
+else
+  # Ubuntu installation
+  apt-get update
+  apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DISTRO stable"
+  apt-get update
+  apt-get install -y docker-ce
+fi
+
+# Install Docker Compose
+echo "Installing Docker Compose"
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Docker CLI
+echo "Installing Docker CLI"
+apt-get install -y docker-ce-cli
 
 # Start and enable Docker service
 systemctl start docker
 systemctl enable docker
 
-# Output Docker version and Docker Compose version
+# Print Docker, Docker CLI, and Docker Compose versions
 docker --version
 docker-compose --version
 
-echo "Docker and Docker Compose have been installed and configured."
+echo "Docker, Docker CLI, and Docker Compose have been successfully installed."
 
-# You may need to log out and log back in for the user group changes to take effect.
