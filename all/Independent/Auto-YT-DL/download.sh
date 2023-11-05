78178e9c3605ac8b884368697a9188b8edde4066
rune <christian.rune1998@gmail.com>
2023-11-05 15:45:39 +0100

    Added a better system for docker download

--- all/Independent/Auto-YT-DL/download.sh
+++ all/Independent/Auto-YT-DL/download.sh
@@ -5,6 +5,9 @@ input_urls=$(cat ~/plex/media/url_file.txt)
 
 output_path=~/plex/media/youtube
 
+# Define the maximum number of running containers
+max_containers=3
+
 # Loop over each URL
 while read -r url; do
     # Set the video file path
@@ -31,13 +34,22 @@ while read -r url; do
     # Create the video folder if it doesn't exist
     mkdir -p "${video_folder}"
 
-    # Download video using docker run command
+    # Check the number of running youtube-dl Docker containers
+    while [ "$(docker ps | grep mikenye/youtube-dl | wc -l)" -ge "$max_containers" ]; do
+        echo "Waiting for available youtube-dl container..."
+        sleep 5
+    done
+
+    # Download video using docker run command in detached mode and delete the container when finished
     docker run \
-        --rm -i \
+        --rm -d \
         -e PGID=$(id -g) \
         -e PUID=$(id -u) \
         -v "$(pwd)":/workdir:rw \
         -v "${video_folder}":/output:rw \
+        --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
+        --cpus 1 \
+        --memory 2g \
         mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o '/output/%(title)s.%(ext)s' "${url}"
 
     echo "Video downloaded successfully!"
