6f0e4dd4f9b896ec58c7e4a40b49ed5d2a4ec40a
rune <christian.rune1998@gmail.com>
2023-11-06 20:42:42 +0100

    Minor updates

--- Independent/Auto-YT-DL/automated-check.sh
+++ Independent/Auto-YT-DL/automated-check.sh
@@ -8,8 +8,7 @@ output_path=~/plex/media/youtube
 # Define the maximum number of running containers
 max_containers=3
 
-
-# Loop through each URL in the input_urls
+# Loop over each URL
 while read -r url; do
     # Set the video file path
     video_folder="${output_path}/$(echo "${url}" | awk -F '=' '{print $2}')"
@@ -18,48 +17,35 @@ while read -r url; do
     # Create the video folder if it doesn't exist
     mkdir -p "${video_folder}"
 
-    # Check if the video file already exists using the archive function in youtube-dl
-    if docker run --rm \
+    # Check the number of running youtube-dl Docker containers
+    while [ "$(docker ps | grep mikenye/youtube-dl | wc -l)" -ge "$max_containers" ]; do
+        echo "Waiting for available youtube-dl container..."
+        sleep 5
+    done
+
+    # Download video using docker run command in detached mode and delete the container when finished
+    docker run \
+        --rm -d \
         -e PGID=$(id -g) \
         -e PUID=$(id -u) \
         -v "$(pwd)":/workdir:rw \
         -v "${video_folder}":/output:rw \
-        mikenye/youtube-dl \
-        --get-filename \
-        --skip-download \
-        --download-archive archive.txt \
+        --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
+        --cpus 1 \
+        --memory 2g \
+        mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
+        --write-info-json \
+        --write-thumbnail \
+        --write-description \
+        --write-sub \
+        --embed-subs \
+        --convert-subs srt \
+        --write-auto-sub \
+        --download-archive ~/plex/media/archive.txt \
         --output '/output/%(title)s.%(ext)s' \
-        "${url}" | grep -qF "$video_file"; then
+        "${url}"
 
-        echo "Video already downloaded: $video_file"
-    else
-        # Download video using docker run command in detached mode and delete the container when finished
-        docker run \
-            --rm -d \
-            -e PGID=$(id -g) \
-            -e PUID=$(id -u) \
-            -v "$(pwd)":/workdir:rw \
-            -v "${video_folder}":/output:rw \
-            --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
-            --cpus 1 \
-            --memory 2g \
-            mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
-            --write-info-json \
-            --write-thumbnail \
-            --write-description \
-            --write-sub \
-            --embed-subs \
-            --convert-subs srt \
-            --write-auto-sub \
-            --download-archive archive.txt \
-            --output '/output/%(title)s.%(ext)s' \
-            "${url}"
-
-        if [ -f "$video_file" ]; then
-            echo "Video downloaded successfully: $video_file"
-        else
-            echo "Failed to download video: $video_file"
-        fi
-    fi
+    echo "Video downloaded successfully!"
 done <<< "$input_urls"
+
 
--- Independent/Auto-YT-DL/download.sh
+++ Independent/Auto-YT-DL/download.sh
@@ -23,47 +23,28 @@ while read -r url; do
         sleep 5
     done
 
-    # Check if the video file already exists using the archive function in youtube-dl
-    if docker run --rm \
+    # Download video using docker run command in detached mode and delete the container when finished
+    docker run \
+        --rm -d \
         -e PGID=$(id -g) \
         -e PUID=$(id -u) \
         -v "$(pwd)":/workdir:rw \
         -v "${video_folder}":/output:rw \
-        mikenye/youtube-dl \
-        --get-filename \
-        --skip-download \
-        --download-archive archive.txt \
+        --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
+        --cpus 1 \
+        --memory 2g \
+        mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
+        --write-info-json \
+        --write-thumbnail \
+        --write-description \
+        --write-sub \
+        --embed-subs \
+        --convert-subs srt \
+        --write-auto-sub \
+        --download-archive ~/plex/media/archive.txt \
         --output '/output/%(title)s.%(ext)s' \
-        "${url}" | grep -qF "$video_file"; then
+        "${url}"
 
-        echo "Video already downloaded: $video_file"
-    else
-        # Download video using docker run command in detached mode and delete the container when finished
-        docker run \
-            --rm -d \
-            -e PGID=$(id -g) \
-            -e PUID=$(id -u) \
-            -v "$(pwd)":/workdir:rw \
-            -v "${video_folder}":/output:rw \
-            --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
-            --cpus 1 \
-            --memory 2g \
-            mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
-            --write-info-json \
-            --write-thumbnail \
-            --write-description \
-            --write-sub \
-            --embed-subs \
-            --convert-subs srt \
-            --write-auto-sub \
-            --download-archive archive.txt \
-            --output '/output/%(title)s.%(ext)s' \
-            "${url}"
-
-        if [ -f "$video_file" ]; then
-            echo "Video downloaded successfully: $video_file"
-        else
-            echo "Failed to download video: $video_file"
-        fi
-    fi
+    echo "Video downloaded successfully!"
 done <<< "$input_urls"
+
