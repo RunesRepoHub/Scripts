1e5b0eb4fc4a9f64b22916992613c6211052cee5
rune <christian.rune1998@gmail.com>
2023-11-06 19:26:09 +0100

    testing

--- Independent/Auto-YT-DL/download.sh
+++ Independent/Auto-YT-DL/download.sh
@@ -13,7 +13,6 @@ while read -r url; do
     # Set the video file path
     video_folder="${output_path}/$(echo "${url}" | awk -F '=' '{print $2}')"
     video_file="${video_folder}/$(echo "${url}" | awk -F '=' '{print $2}').mp4"
-done
 
     # Create the video folder if it doesn't exist
     mkdir -p "${video_folder}"
@@ -25,27 +24,26 @@ done
     done
 
     # Download video using docker run command in detached mode and delete the container when finished
-docker run \
-    --rm -d \
-    -e PGID=$(id -g) \
-    -e PUID=$(id -u) \
-    -v "$(pwd)":/workdir:rw \
-    -v "${video_folder}":/output:rw \
-    --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
-    --cpus 1 \
-    --memory 2g \
-    mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
-    --write-info-json \
-    --write-thumbnail \
-    --write-description \
-    --write-sub \
-    --embed-subs \
-    --convert-subs srt \
-    --write-auto-sub \
-    --output '/output/%(title)s.%(ext)s' \
-    "${url}"
+    docker run \
+        --rm -d \
+        -e PGID=$(id -g) \
+        -e PUID=$(id -u) \
+        -v "$(pwd)":/workdir:rw \
+        -v "${video_folder}":/output:rw \
+        --name "$(echo "${url}" | awk -F '=' '{print $2}')" \
+        --cpus 1 \
+        --memory 2g \
+        mikenye/youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
+        --write-info-json \
+        --write-thumbnail \
+        --write-description \
+        --write-sub \
+        --embed-subs \
+        --convert-subs srt \
+        --write-auto-sub \
+        --output '/output/%(title)s.%(ext)s' \
+        "${url}"
 
     echo "Video downloaded successfully!"
 done <<< "$input_urls"
-
 
