382f76593cde4d8cf1b444b07d4def81ebe3adda
rune <christian.rune1998@gmail.com>
2023-11-06 19:07:26 +0100

    Hotfix auto installer

--- all/Independent/Auto-YT-DL/setup.sh
+++ all/Independent/Auto-YT-DL/setup.sh
@@ -1,7 +1,9 @@
-wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/cleanup.sh
-wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/add-url.sh
-wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/setup-plex.sh
-wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/download.sh
+mkdir -p ~/Auto-YT-DL
+
+curl -o ~/Auto-YT-DL/cleanup.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/cleanup.sh
+curl -o ~/Auto-YT-DL/add-url.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/add-url.sh
+curl -o ~/Auto-YT-DL/setup-plex.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/setup-plex.sh
+curl -o ~/Auto-YT-DL/download.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/download.sh
 
 # Check if ~/plex/media, ~/plex/transcode, and ~/plex/plex/database exist
 if [ ! -d ~/plex/media/youtube ] || [ ! -d ~/plex/transcode ] || [ ! -d ~/plex/library ]; then
