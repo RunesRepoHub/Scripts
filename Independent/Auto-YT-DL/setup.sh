fcd7d956edf723be7449b05b2e37190224c439f7
rune <christian.rune1998@gmail.com>
2023-11-06 21:11:15 +0100

    Hotfix

--- Independent/Auto-YT-DL/setup.sh
+++ Independent/Auto-YT-DL/setup.sh
@@ -19,29 +19,27 @@ NC='\e[0m'  # Reset to default
 ###################
 
 # Make the folder
-echo -e "${Yellow}Setting up Auto-YT-DL...${NC}"
-echo -e "${Yellow}Please wait...${NC}"
+echo -e "${Purple}Setting up Auto-YT-DL...${NC}"
 echo -e "${Yellow}Make the folder ~/Auto-YT-DL${NC}"
 mkdir -p ~/Auto-YT-DL/
 
 sleep 2
 
-echo -e "${Yellow}Downloading files...${NC}"
-echo -e "${Yellow}Please wait...${NC}"
+echo -e "${Purple}Downloading files...${NC}"
 
-curl -o ~/Auto-YT-DL/automated-check.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/automated-check.sh > /dev/null
+curl -s -o ~/Auto-YT-DL/automated-check.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/automated-check.sh > /dev/null
 
-curl -o ~/Auto-YT-DL/cleanup.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/cleanup.sh > /dev/null
+curl -s -o ~/Auto-YT-DL/cleanup.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/cleanup.sh > /dev/null
 
-curl -o ~/Auto-YT-DL/add-url.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/add-url.sh > /dev/null
+curl -s -o ~/Auto-YT-DL/add-url.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/add-url.sh > /dev/null
 
-curl -o ~/Auto-YT-DL/setup-plex.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/setup-plex.sh > /dev/null
+curl -s -o ~/Auto-YT-DL/setup-plex.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/setup-plex.sh > /dev/null
 
-curl -o ~/Auto-YT-DL/download.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/download.sh > /dev/null
+curl -s -o ~/Auto-YT-DL/download.sh https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/Independent/Auto-YT-DL/download.sh > /dev/null
 
 sleep 2
 
-echo -e "${Yellow} Making folders for plex. media, transcode, and library...${NC}"
+echo -e "${Purple}Making folders for plex. media, transcode, and library...${NC}"
 
 # Check if ~/plex/media, ~/plex/transcode, and ~/plex/plex/database exist
 if [ ! -d ~/plex/media/youtube ] || [ ! -d ~/plex/transcode ] || [ ! -d ~/plex/library ]; then
@@ -53,14 +51,12 @@ bash ~/Auto-YT-DL/add-url.sh
 
 sleep 2
 
-echo -e "${Yellow}Setting up plex...${NC}"
+echo -e "${Purple}Setting up plex...${NC}"
 
 bash ~/Auto-YT-DL/setup-plex.sh
 
 
-echo -e "${Yellow}Setup cronjob and alias${NC}"
-
-0 0 * * * bash ~/Auto-YT-DL/automated-check.sh
+echo -e "${Purple}Setup cronjob and alias${NC}"
 
 alias add-url="bash ~/Auto-YT-DL/add-url.sh"
 
