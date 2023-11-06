36bc878b0e8321193d9783cdfb8a84be6df73ddc
rune <christian.rune1998@gmail.com>
2023-11-06 19:05:05 +0100

    Added some more auto install function

--- all/Independent/Auto-YT-DL/setup.sh
+++ all/Independent/Auto-YT-DL/setup.sh
@@ -1,3 +1,8 @@
+wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/cleanup.sh
+wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/add-url.sh
+wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/setup-plex.sh
+wget https://raw.githubusercontent.com/RunesRepoHub/Scripts/Dev/all/Independent/Auto-YT-DL/download.sh
+
 # Check if ~/plex/media, ~/plex/transcode, and ~/plex/plex/database exist
 if [ ! -d ~/plex/media/youtube ] || [ ! -d ~/plex/transcode ] || [ ! -d ~/plex/library ]; then
     # Create the folders if they don't exist
